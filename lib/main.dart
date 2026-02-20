2d50c63b74011c4c2a547dbc2e4bc0cfabb42cbb
jih7ne <jianmercley@gmail.com>
2026-02-20 23:23:35 +0000

    refactor the text into a new widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,17 +37,18 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+       var pair = appState.current;  
 
      return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair), 
 
        
     ElevatedButton(
       onPressed: () {
-        appState.getNext();  // ← This instead of print().
+        appState.getNext();  
       },
       child: Text('Next'),
     ),
@@ -55,5 +56,19 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(pair.asLowerCase);
   }
 }
